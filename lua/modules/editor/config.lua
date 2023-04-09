f8ee2bdedd71d300f96310d7e04f5d08c138393b
luyanfcp <luyanfcp@foxmail.com>
2023-04-09 22:57:58 +0800

    feat: add basic config to neovim from dope

--- lua/modules/editor/config.lua
+++ lua/modules/editor/config.lua
@@ -1,26 +1,28 @@
 local config = {}
 
 function config.nvim_treesitter()
-  vim.api.nvim_command('set foldmethod=expr')
-  vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
-  require('nvim-treesitter.configs').setup({
-    ensure_installed = 'all',
-    ignore_install = { 'phpdoc' },
-    highlight = {
-      enable = true,
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        keymaps = {
-          ['af'] = '@function.outer',
-          ['if'] = '@function.inner',
-          ['ac'] = '@class.outer',
-          ['ic'] = '@class.inner',
+    vim.api.nvim_command('set foldmethod=expr')
+    vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
+    require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+            'bash', 'c', 'cpp', 'css', 'dockerfile', 'go', 'gomod', 'html',
+            'javascript', 'json', 'lua', 'python', 'regex', 'rust', 'toml',
+            'tsx', 'typescript', 'yaml', 'markdown'
         },
-      },
-    },
-  })
+        ignore_install = {'phpdoc'},
+        highlight = {enable = true},
+        textobjects = {
+            select = {
+                enable = true,
+                keymaps = {
+                    ['af'] = '@function.outer',
+                    ['if'] = '@function.inner',
+                    ['ac'] = '@class.outer',
+                    ['ic'] = '@class.inner'
+                }
+            }
+        }
+    })
 end
 
 return config
--- lua/modules/ui/package.lua
+++ lua/modules/ui/package.lua
@@ -1,18 +1,22 @@
 local package = require('core.pack').package
 local conf = require('modules.ui.config')
 
-package({ 'glepnir/zephyr-nvim', config = conf.zephyr })
+package({'glepnir/zephyr-nvim', config = conf.zephyr})
 
-package({ 'glepnir/dashboard-nvim', config = conf.dashboard })
+package({'glepnir/dashboard-nvim', config = conf.dashboard})
 
 package({
-  'akinsho/nvim-bufferline.lua',
-  config = conf.nvim_bufferline,
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
+    'akinsho/nvim-bufferline.lua',
+    config = conf.nvim_bufferline,
+    dependencies = {'nvim-tree/nvim-web-devicons'}
 })
 
 package({
-  'lukas-reineke/indent-blankline.nvim',
-  event = 'BufRead',
-  config = conf.indent_blankline,
+    'lukas-reineke/indent-blankline.nvim',
+    event = 'BufRead',
+    config = conf.indent_blankline
 })
+
+package({'nvim-lualine/lualine.nvim', config = conf.lualine})
+
+package({'nvim-tree/nvim-tree.lua', config = conf.nvim_tree})
